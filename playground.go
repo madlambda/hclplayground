c7ec5ed015b4e0643f2d5aaba11176b1591fa4ca
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-03-20 17:12:01 +0100

    feat: we have validation working

--- playground.go
+++ playground.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strings"
+	"syscall/js"
+
+	"github.com/hashicorp/hcl/v2/hclparse"
+)
 
 func main() {
-	fmt.Println("hello")
+	log.Println("starting playground")
+
+	document := js.Global().Get("document")
+	codeInput := document.Call("getElementById", "code")
+	formattedOutput := document.Call("getElementById", "formatted")
+	errorsOutput := document.Call("getElementById", "errors")
+	validateButton := document.Call("getElementById", "validate")
+
+	fmt.Println(codeInput.Get("value"))
+	fmt.Println(formattedOutput.Get("value"))
+	formattedOutput.Set("value", "hallo")
+
+	buttonClicks := make(chan struct{})
+	validateButton.Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) any {
+		log.Println("button clicked")
+		buttonClicks <- struct{}{}
+		return nil
+	}))
+
+	for range buttonClicks {
+		log.Println("received button clicked msg")
+
+		hclInput := []byte(codeInput.Get("value").String())
+		p := hclparse.NewParser()
+		_, diags := p.ParseHCL(hclInput, "playground")
+
+		errMsgs := make([]string, len(diags))
+
+		for i, diag := range diags {
+			errMsgs[i] = diag.Error()
+		}
+
+		errorsOutput.Set("value", strings.Join(errMsgs, "\n"))
+	}
 }
